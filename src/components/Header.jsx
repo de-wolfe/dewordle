53f881d0e5babb3bfd848522077ed4ac9a5a477d
Chester de Wolfe <Chester.dewolfe@gmail.com>
2024-12-15 18:15:54 -0500

    First changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,11 @@
-import {IconGithub, IconHelp} from '@icons';
-import {ExternalLink, ThemeButton, HelpModal} from '@components';
-import {useModal} from '@hooks';
+import { IconGithub, IconHelp } from '@icons';
+import { ExternalLink, ThemeButton, HelpModal } from '@components';
+import { useModal } from '@hooks';
 
 export function Header() {
-  const {setModalContent} = useModal();
+  const { setModalContent } = useModal();
   return (
     <header className="flex h-header w-full items-center justify-center border-b border-gray-30 px-3 text-4xl dark:border-black-10">
-      <ExternalLink href="https://github.com/shanpriyan/wordle-clone">
-        <IconGithub className="h-7 w-7 opacity-100 hover:opacity-60" />
-      </ExternalLink>
       <IconHelp
         className="ml-2 h-7 w-7 cursor-pointer opacity-100 hover:opacity-80"
         onClick={() =>
